9932940322c96b97fb65a0c711d26aa62dee7d56
Hundter Biede <hbiede@gmail.com>
2019-07-21 21:37:38 -0500

    Ignore case

--- examples/grade.inc.php
+++ examples/grade.inc.php
@@ -248,7 +248,7 @@ abstract class Tester {
         $result .= "</div>"; //end of float: left
 	$result .= "</div>"; //end of clear: both
 	$executionOutput = $this->executeCommands();
-	similar_text(preg_replace("/\s/", "", $this->expectedOutput), preg_replace("/\s/", "", $executionOutput), $similarity); // compare the output to the expectation, ignoring whitespace
+	similar_text(preg_replace("/\s/", "", strtolower($this->expectedOutput)), preg_replace("/\s/", "", strtolower($executionOutput)), $similarity); // compare the output to the expectation, ignoring whitespace and case sensetivity
 	$result .= "<h4>Output Similarity: <span style='color: #".($similarity==100 ? "0f0" : dechex($similarity/100*0x10000)).";'>".number_format($similarity, 2, '.', '')."%</span></h4>\n";
 	$result .= $executionOuput;
     } else {
