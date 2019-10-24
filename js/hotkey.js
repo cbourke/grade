edb9021da513b343635f1381d9038e9d9e95833c
Hundter Biede <hbiede@me.com>
2019-10-24 18:48:09 -0500

    Run immediately

--- js/hotkey.js
+++ js/hotkey.js
@@ -1,6 +1,8 @@
-document.onkeyup = function(e) {
-  if (e.altKey && e.which == 71) {
-    // Alt + G
-    $("#submitButton").click();
-  }
-};
+(function() {
+  document.onkeyup = function(e) {
+    if (e.altKey && e.which == 71) {
+      // Alt + G
+      $("#submitButton").click();
+    }
+  };
+})();
