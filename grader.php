f292ede221cabb6551e4cbcc8adcaec8dc5c0437
Chris Bourke <cbourke@cse.unl.edu>
2020-01-26 14:30:45 -0600

    updated grader-facing interface to use bootstrap, new prettifier and use line-breakable pre tag

--- grader.php
+++ grader.php
@@ -5,16 +5,23 @@ $course = Course::createCourse($config['homework_file']);
 $roster = Roster::createRoster($config['mail_file']);
 
 ?>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Grading Checker</title>
 
-  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
-  <link href="google-code-prettify-minified/prettify.css" type="text/css" rel="stylesheet" />
-  <script type="text/javascript" src="google-code-prettify-minified/prettify.js"></script>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+
+  <script src="./google-code-prettify/run_prettify.js" defer></script>
+
   <script src="js/hotkey.js"></script>
 
 <script type="text/javascript">
@@ -62,7 +69,7 @@ function loadIt() {
     success: function (data) {
       $('#grade_content').html(data);
       $(document).prop('title', '\u2714 ' + title);
-      prettyPrint();
+      PR.prettyPrint();
     },
     error: function (xhr, statusText) {
       $(document).prop('title', '\u2716 ' + title);
@@ -74,53 +81,74 @@ function loadIt() {
 
 }
 </script>
+
+<style type="text/css">
+
+/* Necessary for wrapping long lines, 
+   however it only breas on whitespace;
+   very long lines of "data" will still 
+   extend past the pre area.  */
+pre {
+  white-space: pre-wrap;
+  border: 1px solid #d3d3d3;
+  padding: 10px;
+}
+
+/* Necessary for line numbering on every line */
+li.L0, li.L1, li.L2, li.L3,
+li.L5, li.L6, li.L7, li.L8 {
+  list-style-type: decimal !important;
+}
+</style>
+
 </head>
 
-<body onload="redirectHTTPS()">
-<h1><?php print $course->getCourseNumber(); ?> Program Grade Checker - Grader Interface</h1>
+<body class="container-fluid" onload="redirectHTTPS()">
+<h2><?php print $course->getCourseNumber(); ?> Program Grade Checker - Grader Interface</h2>
 
 <p>This interface is for graders</p>
 
-<h2>Grade A Student's Program</h2>
-<form id="grade_form" name="grade_form" action="" method="get">
-<div style="margin: 1em;">
-  <table>
-    <tr>
-      <td>CSE Login</td>
-      <td><?php print $course->getCourseNumber(); ?></td>
-    </tr>
-    <tr>
-      <td>CSE Password</td>
-      <td><input type="password" name="cse_password"/></td>
-    </tr>
-    <tr>
-      <td>Student CSE Login</td>
-      <td><input type="text" name="student_cse_login" id="student_cse_login" /></td>
-    </tr>
-    <tr>
-      <td>Assignment</td>
-<td>
-<select name="hw_num">
-<?php
-  foreach ($course->getAssignments() as $assign) {
-   print "<option value=\"".$assign->getNumber()."\" ";
-   if(!$assign->hasGraderGradeScript()) {
-     print " disabled=\"disabled\"";
-   }
-   print ">".$assign->getNumber()." ".$assign->getLabel()."</option>\n";
-  }
-?>
-</select>
-</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td><input id="submitButton" type="button" value = "Grade Them!" onclick="loadIt();"/>
-      </td>
-    </tr>
+<h4>Grade a Student's Program</h4>
 
-  </table>
-</form>
+<div class="col-md-6">
+  <form id="grade_form" name="grade_form" action="" method="get">
+    <div class="form-group row">
+      <label for="cseLogin" class="col-sm-3 col-form-label">CSE Login</label>
+      <div class="col-sm-9">
+        <input class="form-control" type="text" placeholder="<?php print $course->getCourseNumber(); ?>" id="cseLogin" readonly/>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="cse_password" class="col-sm-3 col-form-label">Password</label>
+      <div class="col-sm-9">
+        <input class="form-control" type="password" name="cse_password" id="cse_password"/>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="student_cse_login" class="col-sm-3 col-form-label">Student CSE Login</label>
+      <div class="col-sm-9">
+        <input class="form-control" type="text" name="student_cse_login" id="student_cse_login"/>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="hw_num" class="col-sm-3 col-form-label">Assignment</label>
+      <div class="col-sm-9">
+        <select name="hw_num" class="form-control" id="hw_num">
+          <?php
+            foreach ($course->getAssignments() as $assign) {
+             print "<option value=\"".$assign->getNumber()."\" ";
+             if(!$assign->hasGraderGradeScript()) {
+               print " disabled=\"disabled\"";
+             }
+             print ">".$assign->getNumber()." ".$assign->getLabel()."</option>\n";
+            }
+          ?>
+        </select>
+      </div>
+    </div>
+    <button id="submitButton" type="button" class="btn btn-primary" onclick="loadIt();">Submit</button>
+  </form>
+</div>
 
 <h1 id="results_header"></h1>
 <div id="results"></div>
